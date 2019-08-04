4602f6b9140a450a18db62905c2bf348752d5968
NettoVieira <joao.fvneto@hotmail.com>
2019-08-04 18:13:55 -0300

    Desafio: filtrando funcionarios apartir de uma requisição utilizando o Axios

---
+++ funcionarios/funcionarios.js
@@ -0,0 +1,18 @@
+
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+const filtraPais = funcionario => funcionario.pais === 'China' 
+const filtraGenero = funcionario => funcionario.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+  return func.salario < funcAtual.salario ? func : funcAtual
+}
+
+axios.get(url).then(response => {
+  const funcionarios = response.data
+
+  console.log(funcionarios.filter(filtraGenero).filter(filtraPais).reduce(menorSalario))
+})
+
+
+
